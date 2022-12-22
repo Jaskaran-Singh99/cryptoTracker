aeac2e1b733d947160b03c7d50629a58543c6f53
Jaskaran <jaskaran7087065314@gmail.com>
2022-12-22 07:50:45 +0530

    Exchanges.js

--- src/components/exchanges.jsx
+++ src/components/exchanges.jsx
@@ -2,7 +2,8 @@ import axios from 'axios'
 import server from '../index'
 import React, {useEffect, useState} from 'react'
 import Loader from '../components/loader'
-import { Container, HStack } from '@chakra-ui/react'
+import { Container, HStack, Text, Image, VStack, Heading } from '@chakra-ui/react'
+
 const Exchanges =()=>{
     const [loader, setLoader] = useState(true)
     const [exchanges, setExchanges] = useState([])
@@ -21,18 +22,47 @@ const Exchanges =()=>{
       
     
     return(
-        <Container>
+        <Container maxWidth={'1300px'}>
+        <>
             {loader ? <Loader></Loader> : (<>
-            <HStack>
+            <HStack wrap={'wrap'} justifyContent={'center'}>
                 {exchanges.map((i)=>(
-                    <div>{i.name}</div>
+                    <>
+                        <a href={i.url} target={'_blank'}>           
+                            <VStack w={'44'} shadow={'lg'} borderRadius={'lg'} p={'8'} m={'4'} transition={'all 0.2s'}
+                            css={{
+                                '&:hover':{
+                                    transform:'scale(1.1)'
+                                }
+                            }}>
+                                <Image src={i.image} w={'10'} h={'10'} objectFit={'contain'} alt={'Exchanges'}></Image>
+
+                                <Heading noOfLines={'1'} size={'md'}>
+                                    {i.trust_score_rank}
+                                </Heading>
+
+                                <Text noOfLines={1}>
+                                    {i.name}
+                                </Text>
+                            </VStack>
+                        </a>
+                       
+                    </>
+                    
+
+
+                  
                 ))}
                 
             </HStack>
 
             </>) }
-        </Container>
+        </>
+      </Container>
+        
+        
     )
+    
 }
 
 export default Exchanges
